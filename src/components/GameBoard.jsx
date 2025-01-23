854980600efe30264c51d2d960e02a4f41e8645b
Irinamiszczanka <miszczankairina@gmail.com>
2025-01-23 12:13:33 +0100

    added component Log

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,13 +1,19 @@
 
-
-
 const initialGameBoard = [
 	[null, null, null],
 	[null, null, null],
 	[null, null, null]
 ];
 
-export default function GameBoard({ onSelectSquare }) {
+export default function GameBoard({ onSelectSquare, turns }) {
+	let gameBoard = initialGameBoard;
+
+	for (const turn of turns) {
+		const { square, player } = turn;
+		const { row, col } = square;
+
+		gameBoard[row][col] = player;
+	}
 	// const [ gameBoard, setGameBoard ] = useState(initialGameBoard);
 
 	// function handleSelectSquare(rowIndex, colIndex) {
@@ -21,11 +27,19 @@ export default function GameBoard({ onSelectSquare }) {
 	// }
 
 
-	return <ol id="game-board">
-		{gameBoard.map((row,rowIndex) => <li key={rowIndex}>
-			<ol>
-				{row.map((playerSymbol, colIndex) => <li key={colIndex}><button onClick={onSelectSquare}>{playerSymbol}</button></li>)}
-			</ol>
-		</li>)}
-	</ol>;
+	return (
+		<ol id="game-board">
+			{gameBoard.map((row,rowIndex) => (
+				<li key={rowIndex}>
+					<ol>
+						{row.map((playerSymbol, colIndex) => 
+							<li key={colIndex}>
+								<button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+							</li>
+						)}
+					</ol>
+				</li>
+			))}
+		</ol>
+	);
 }
